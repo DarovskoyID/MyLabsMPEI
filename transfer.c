9487b75ea2aebfca23b4674dd72e6f71608b2f11
ivandarovskoy <darovskoy.ivan@yandex.ru>
2025-09-15 06:55:28 +0300

    normalize input

--- transfer.c
+++ transfer.c
@@ -5,9 +5,7 @@ void transferFileToBFile(FILE *fileText, FILE *fileBinary) {
     struct record rec;
     char buffer[10];
     while (fgets(buffer, sizeof(buffer), fileText) != NULL) {
-        if(!feof(fileText)){
-            sscanf(buffer, "%c %c %c", &rec.a, &rec.b, &rec.c);
-            fwrite(&rec, sizeof(struct record), 1, fileBinary);
-        }
+        sscanf(buffer, "%c %c %c", &rec.a, &rec.b, &rec.c);
+        fwrite(&rec, sizeof(struct record), 1, fileBinary);
     }
 }
