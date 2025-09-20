cb81d710dceb1a7b7006ec34058c2859617b2e90
ivandarovskoy <darovskoy.ivan@yandex.ru>
2025-09-20 22:01:18 +0300

    temper

---
+++ unitStatic.c
@@ -0,0 +1,4 @@
+//
+// Created by Иван on 20.09.2025.
+//
+#include "unitStatic.h"
---
+++ unitStatic.h
@@ -0,0 +1,13 @@
+//
+// Created by Иван on 20.09.2025.
+//
+
+#ifndef UNITSTATIC_H
+#define UNITSTATIC_H
+#include <stdio.h>
+
+void findMinArray(int mat[][], int array[], int sizeX, int sizeY);
+void OutputArray(int array[], int size);
+int FindZero(int matrix[][], int sizeX, int sizeY);
+void inputMatrixFromFile(int matrix[][], int *sizeX, int*sizeY, FILE *file);
+#endif //UNITSTATIC_H
