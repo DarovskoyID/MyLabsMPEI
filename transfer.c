022d8798725df64cd77c5636bcebdf697c99da78
ivandarovskoy <darovskoy.ivan@yandex.ru>
2025-09-26 11:43:05 +0300

    1 edit

--- transfer.c
+++ transfer.c
@@ -8,11 +8,18 @@ void transferFileToBFile(FILE *fileText, FILE *fileBinary, int *flag) {
 
     while (fgets(buffer, sizeof(buffer), fileText) != NULL) {
         int j = 0;
-        for (int i = 0; buffer[i] != '\0' && j < 3; i++) {
-            if ((buffer[i] >= 'A' && buffer[i] <= 'Z') || (buffer[i] >= 'a' && buffer[i] <= 'z')){
+        int i = 0;
+        while (buffer[i] != '\0' && j < 3) {
+            if ((buffer[i] != ' ' && buffer[i] != '\n' && buffer[i] != '\t') &&
+                (buffer[i+1] == ' ' || buffer[i+1] == '\n' || buffer[i+1] == '\t' || buffer[i+1] == '\0') &&
+                (buffer[i-1] == ' ' || buffer[i-1] == '\n' || buffer[i-1] == '\t' || buffer[i-1] == '\0')
+                )
+
+                {
                 rec[j] = buffer[i];
                 j++;
             }
+            i++;
         }
 
         if (j == 3) {
