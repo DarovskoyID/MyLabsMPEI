ac869e201cbf0f4df6f9d223db2189d0cd81afb9
ivandarovskoy <darovskoy.ivan@yandex.ru>
2025-10-08 11:21:28 +0300

    bit sdvig normalize type

--- unit.h
+++ unit.h
@@ -7,11 +7,12 @@
 #include <stdio.h>
 
 #define SIZEOFX 8
-#define BITS (sizeof(unsigned int) * SIZEOFX)
+#define TYPEBIN u_int16_t
+#define BITS (sizeof(TYPEBIN) * SIZEOFX)
 
-unsigned int rotateRight(unsigned int x, int n);
-unsigned int rotateLeft(unsigned int x, int n);
-void PrintBinary(unsigned int x);
+unsigned int rotateRight(TYPEBIN x, int n);
+unsigned int rotateLeft(TYPEBIN x, int n);
+void PrintBinary(TYPEBIN x);
 unsigned int binaryToDecimal(const char *s);
 
 #endif //UNTITLED3_UNIT_H
