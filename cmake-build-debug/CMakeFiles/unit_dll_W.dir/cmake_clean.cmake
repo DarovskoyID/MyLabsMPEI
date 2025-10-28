61fcef838b06600915f38beee9fd1b3e34ff27d0
DarovskoyID <darovskoy.ivan@yandex.ru>
2025-10-28 19:02:12 +0300

    t

---
+++ cmake-build-debug/CMakeFiles/unit_dll_W.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unit_dll_W.dir/unit_dll_W.cpp.obj"
+  "CMakeFiles/unit_dll_W.dir/unit_dll_W.cpp.obj.d"
+  "unit_dll_W.dll"
+  "unit_dll_W.dll.manifest"
+  "unit_dll_W.lib"
+  "unit_dll_W.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unit_dll_W.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
